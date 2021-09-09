6e07f5ff8f5bf3cb3ba83c13a658ef399ac16604
outlawostinato <outlawostinato@gmail.com>
2021-09-09 17:58:56 +0000

    Really fix typo
    Signed-off-by: qubitrenegade <qubitrenegade@gmail.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-extend MyFirewall::FooHelpers
+extend MyFirewall::Helpers
 
 iptables_packages 'install iptables'
 
